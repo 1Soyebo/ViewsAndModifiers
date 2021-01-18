4c62d9e79a396663e742913fb80ec2306f1c2410
1Soyebo <soyeboibukun@gmail.com>
2021-01-18 19:02:43 +0100

    added watermark

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -25,6 +25,27 @@ extension View {
     }
 }
 
+///extreme view modifiers
+struct Watermark: ViewModifier {
+    var text: String
+
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(Color.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        self.modifier(Watermark(text: text))
+    }
+}
 
 //View Compositions
 struct CapsuleText: View {
@@ -49,6 +70,7 @@ struct ContentView: View {
         VStack(spacing: 10){
             motto2
                 .titleStyle()
+                .watermarked(with: "wow")
         }
         
         
